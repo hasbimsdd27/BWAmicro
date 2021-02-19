a7bb34fdc102ed0ef2f5f3859eba7d3247e3c870
Hasbi Musaddad <musaddadhasbi@gmail.com>
2021-02-19 08:15:51 +0700

    add model, migration and controller

--- backend/serviceCourse/routes/api.php
+++ backend/serviceCourse/routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('mentors', 'MentorController@create');
+Route::put('mentors/{id}', 'MentorController@update');
+Route::get('mentors', 'MentorController@index');
+Route::get('mentors/{id}', 'MentorController@show');
